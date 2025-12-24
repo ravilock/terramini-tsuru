7c68da1dffd494864612f41a1937f2be4469f0bc
ravilock <ravi.me.professional@hotmail.com>
2025-12-24 15:01:40 -0300

    feat: tsuru-api should use mongodb

---
+++ modules/tsuru-api/mongodb.tf
@@ -0,0 +1,100 @@
+resource "kubernetes_stateful_set" "mongodb" {
+  metadata {
+    name      = "tsuru-mongodb"
+    namespace = "tsuru"
+    labels = {
+      app = "tsuru-mongodb"
+    }
+  }
+
+  spec {
+    service_name = "tsuru-mongodb"
+    replicas     = 1
+
+    selector {
+      match_labels = {
+        app = "tsuru-mongodb"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "tsuru-mongodb"
+        }
+      }
+
+      spec {
+        container {
+          name  = "mongodb"
+          image = "mongo:7.0"
+
+          port {
+            container_port = 27017
+            name           = "mongodb"
+          }
+
+          env {
+            name  = "MONGO_INITDB_DATABASE"
+            value = "tsuru"
+          }
+
+          volume_mount {
+            name       = "mongodb-data"
+            mount_path = "/data/db"
+          }
+
+          resources {
+            requests = {
+              memory = "256Mi"
+              cpu    = "250m"
+            }
+            limits = {
+              memory = "512Mi"
+              cpu    = "500m"
+            }
+          }
+        }
+      }
+    }
+
+    volume_claim_template {
+      metadata {
+        name = "mongodb-data"
+      }
+
+      spec {
+        access_modes = ["ReadWriteOnce"]
+        resources {
+          requests = {
+            storage = "2Gi"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "mongodb" {
+  metadata {
+    name      = "tsuru-mongodb"
+    namespace = "tsuru"
+    labels = {
+      app = "tsuru-mongodb"
+    }
+  }
+
+  spec {
+    selector = {
+      app = "tsuru-mongodb"
+    }
+
+    port {
+      port        = 27017
+      target_port = 27017
+      name        = "mongodb"
+    }
+
+    type = "NodePort"
+  }
+}
